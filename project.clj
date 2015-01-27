22873aa5bcc39f5f9b0af164b4e29d00aac0da91
niquola <niquola@gmail.com>
2015-01-28 00:09:33 +0300

    update readme & deploy to clojars

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject fhir "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "FHIR client in clojure"
+  :url "https://github.com/fhirbase/fhir.clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
