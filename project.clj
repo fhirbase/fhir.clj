f1787c70cfa614787a9ee6251081d8201c4464cb
niquola <niquola@gmail.com>
2015-08-04 10:15:01 +0000

    fix project.clj

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :resource-paths ["resources"]
+  :repositories  [["clojars"  {:url "https://clojars.org/repo" :sign-releases false}]]
   :plugins  [[lein-cljfmt "0.1.3"]
              [jonase/eastwood "0.2.1"]]
   :dependencies [[org.clojure/clojure "1.6.0"]
